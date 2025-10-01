ed4f6e0469be3613688fe663424f28dc1a4e90b3
TrueGiles <nolankolb818@gmail.com>
2025-10-01 21:42:44 +0200

    start & europe

---
+++ changeImage.js
@@ -0,0 +1,55 @@
+//EUropa
+
+const EU1 = ["/assets\\Pfailnater.jpeg","/assets\\Pfailnater_2.jpeg","/assets\\Pfailnater_3.Jpeg"]
+let EU1N = 0;
+function changeImageEU1(){
+++EU1N;
+if(EU1N >= 3){
+    EU1N = 0;
+}
+document.getElementById("EU1").src = EU1[EU1N]; 
+}
+
+const EU2 = ["/assets\\Huffeisennater.jpg","/assets\\Huffeisennater_2.jpg","/assets\\Huffeisennater_3.jpg"]
+let EU2N = 0;
+function changeImageEU2(){
+++EU2N;
+if(EU2N >= 3){
+    EU2N = 0;
+}
+document.getElementById("EU2").src = EU2[EU2N] 
+}
+
+//asien
+
+const AS1 = ["/assets\\Pfailnater.jpeg","/assets\\Pfailnater_2.jpeg","/assets\\Pfailnater_3.Jpeg"]
+let AS1N = 0;
+function changeImageAS1(){
+++AS1N;
+if(AS1N >= 3){
+    EU1N = 0;
+}
+document.getElementById("AS1").src = AS1[AS1N]; 
+}
+
+const AS2 = ["/assets\\Pfailnater.jpeg","/assets\\Pfailnater_2.jpeg","/assets\\Pfailnater_3.Jpeg"]
+let AS2N = 0;
+function changeImageAS2(){
+++AS2N;
+if(AS2N >= 3){
+    AS2N = 0;
+}
+document.getElementById("AS2").src = AS2[ASN] 
+}
+
+const AS3 = ["/assets\\Pfailnater.jpeg","/assets\\Pfailnater_2.jpeg","/assets\\Pfailnater_3.Jpeg"]
+let AS3N = 0;
+function changeImageAS3(){
+++AS3N;
+if(AS3N >= 3){
+    AS3N = 0;
+}
+document.getElementById("AS3").src = AS3[AS3N]; 
+}
+
+
