8d9baa4a636bdd89692eb15edc215d06724f613d
TrueGiles <nolankolb818@gmail.com>
2025-10-01 22:32:00 +0200

    hopefully final commit

--- changeImage.js
+++ changeImage.js
@@ -1,5 +1,5 @@
 //EUropa
-
+//i know this sucks but im to lazy
 const EU1 = ["/assets\\Pfailnater.jpeg","/assets\\Pfailnater_2.jpeg","/assets\\Pfailnater_3.Jpeg"]
 let EU1N = 0;
 function changeImageEU1(){
@@ -22,34 +22,23 @@ document.getElementById("EU2").src = EU2[EU2N]
 
 //asien
 
-const AS1 = ["/assets\\Pfailnater.jpeg","/assets\\Pfailnater_2.jpeg","/assets\\Pfailnater_3.Jpeg"]
+const AS1 = ["/assets\\Gemeine_Sandrasselotter.jpg","/assets\\Gemeine_Sandrasselotter_2.jpg","/assets\\Gemeine_Sandrasselotter_3.jpg"]
 let AS1N = 0;
 function changeImageAS1(){
 ++AS1N;
 if(AS1N >= 3){
-    EU1N = 0;
+    AS1N = 0;
 }
 document.getElementById("AS1").src = AS1[AS1N]; 
 }
 
-const AS2 = ["/assets\\Pfailnater.jpeg","/assets\\Pfailnater_2.jpeg","/assets\\Pfailnater_3.Jpeg"]
+const AS2 = ["/assets\\kobra_1.jpg","/assets\\kobra_2.jpg","/assets\\kobra_3.jpg"]
 let AS2N = 0;
 function changeImageAS2(){
 ++AS2N;
 if(AS2N >= 3){
     AS2N = 0;
 }
-document.getElementById("AS2").src = AS2[ASN] 
+document.getElementById("AS2").src = AS2[AS2N] 
 }
-
-const AS3 = ["/assets\\Pfailnater.jpeg","/assets\\Pfailnater_2.jpeg","/assets\\Pfailnater_3.Jpeg"]
-let AS3N = 0;
-function changeImageAS3(){
-++AS3N;
-if(AS3N >= 3){
-    AS3N = 0;
-}
-document.getElementById("AS3").src = AS3[AS3N]; 
-}
-
 
